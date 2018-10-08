1ba1b9888e40dc95ca21df19d7fb0457522ecdff
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-08 14:57:30 +0400

    add task 10

---
+++ study-C-sharp/Task_10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int n = 0; n <= num; n++)
+            {
+                sum += (num + n) * (num + n);
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
+
