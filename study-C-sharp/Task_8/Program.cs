c26e0a4d7e4cbbe30750ed0e52dc854b5e6c9a2e
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-08 14:33:17 +0400

    add task 8

---
+++ study-C-sharp/Task_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter first number ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("enter second number ");
+            int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            
+            for (int n = firstNum; n <= secondNum; n++)
+            {
+                
+                if (n % 3 == 0)
+                {
+                    Console.WriteLine(n);
+                    break;
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
+
