58f97da3059df114d3b6af9f7fc17f51926ddf60
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-08 16:10:05 +0400

    add task 12

--- study-C-sharp/Task_10/Program.cs
+++ study-C-sharp/Task_10/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Task_10
+namespace Task_11
 {
     class Program
     {
---
+++ study-C-sharp/Task_12/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Խնդիր_12:
+
+Տրված է N բնական թիվը։ Հաշվել 1.1 – 1.2 + 1.3 – … արտահայտության արժեքը։ 
+Նշանափոխ գումարելիների քանակը N է։ Պայմանական (if) օպերատոր չօգտագործել։
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            double sum = 0;
+            double first = 1.1;
+
+            for (int i = 0; i < num; ++i)
+                sum += Math.Pow(-1, i) * (first += 0.1);
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
