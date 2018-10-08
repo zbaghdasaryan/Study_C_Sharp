1223849f32bea8f315a3c0a07ae807f3ecb69071
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-08 11:52:18 +0400

    Add Project Task_1

---
+++ study-C-sharp/Task_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; true;)
+            {
+                Console.Write("enter any number ");
+                ulong num = Convert.ToUInt64(Console.ReadLine());
+                ushort sum = 0;
+
+                for (; num > 0;)
+                {
+                    sum += (ushort)(num % 10);
+                    Console.Write(num % 10 + " + ");
+                    num /= 10;
+
+                }
+                Console.CursorLeft = Console.CursorLeft - 2;
+                Console.WriteLine("= " + sum);
+            }
+        }
+    }
+}
+
---
+++ study-C-sharp/study-C-sharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study_C_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
