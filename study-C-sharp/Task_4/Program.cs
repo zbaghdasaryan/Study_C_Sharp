4249843341e2a620ab321750c14a5922067c963c
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-08 12:43:06 +0400

    add task 4

---
+++ study-C-sharp/Task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number ");
+            double num = Convert.ToDouble(Console.ReadLine());
+
+            for (int n = 1; n < 9; n++)
+            {
+                Console.WriteLine(Math.Pow(num, n));
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
