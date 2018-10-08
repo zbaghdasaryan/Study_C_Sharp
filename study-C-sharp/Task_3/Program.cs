3ce9d12f470ba5d417ac7fb2eb4268440dbd144f
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-08 12:22:56 +0400

    add task 3

---
+++ study-C-sharp/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number ");
+            int firstNum = Convert.ToInt32(Console.ReadLine());
+            for (int K = 0; K < firstNum; K++)
+            {
+                Console.WriteLine(K);
+             
+            }
+            Console.ReadKey();
+        }
+    }
+}
