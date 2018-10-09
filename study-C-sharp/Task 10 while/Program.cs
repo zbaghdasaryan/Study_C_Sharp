f20ba72a3744730dbcde154efdeed0303057c8a1
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-09 15:38:12 +0400

    add Task 11 while

--- study-C-sharp/Task 10 while/Program.cs
+++ study-C-sharp/Task 10 while/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+Խնդիր_10:
+Տրված է N բնական թիվը։ Հաշվել N2 + (N + 1)2 + (N + 2)2 + … + (2·N)2 գումարը։
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
