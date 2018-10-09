c510b8d25b2ea4cdb7f490e39f9417743f86039e
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-09 15:26:29 +0400

    add Task 10

---
+++ study-C-sharp/Task 10 while/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_10_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter number ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+            int n = 0;
+            while (n<=num)
+             {
+                sum += (num + n) * (num + n);
+                n++;
+             }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
