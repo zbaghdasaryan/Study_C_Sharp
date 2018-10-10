6d6362a2e0afe21a0b6a47d2dd6ec479deb792f3
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-10 16:48:08 +0400

    add Task 14 while

---
+++ study-C-sharp/Task 14 while/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace task_14_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; true;)
+            {
+                Console.Write("enter any number ");
+                ulong num = Convert.ToUInt64(Console.ReadLine());
+                ushort sum = 0;
+
+                while (num > 0)
+
+                {
+                    sum += (ushort)(num % 10);
+                    Console.Write(num % 10 + " + ");
+                    num /= 10;
+
+                }
+                Console.CursorLeft = Console.CursorLeft - 2;
+                Console.WriteLine("= " + sum);
+                if ((int)num==2*sum)
+                {
+                    Console.WriteLine("yes");
+                }
+                else
+                    Console.WriteLine("no");
+            }
+        }
+    }
+}
--- study-C-sharp/romb/Program.cs
+++ study-C-sharp/romb/Program.cs
@@ -4,30 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace romb
+namespace PictureApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int l = Convert.ToInt32(Console.ReadLine());
-       
-            
-            for (int i = 0; i <= l; i++)
+            int N = 0; //Rhombus side
+            N = Convert.ToInt32(Console.ReadLine());
+            string visible = " + ";
+            string unvisible = "   ";
+            //Full Rhombus
+            for (int i = 0; i < N; i = Math.Abs(i) + 1)
             {
-                Console.SetCursorPosition(l-i, i);
-                Console.WriteLine('*');
-
-                Console.SetCursorPosition(l + i, i);
-                Console.WriteLine('*');
-
-                Console.SetCursorPosition(l - i, 2*l - i);
-                Console.WriteLine('*');
-
-                Console.SetCursorPosition(l + i, 2*l - i);
-                Console.WriteLine('*');
+                for (int j = 1; j <= 2 * N - 1; j++)
+                {
+                    Console.Write((j >= N - i && j <= N + i) ? visible : unvisible);
+                }
+                Console.WriteLine();
             }
+            for (int p = 1; p <= N - 1; p++)
+            {
+                for (int k = 1; k <= 2 * N - 1; k++)
+                {
+                    Console.Write((k > p && k < 2 * N - p) ? visible : unvisible);
+                }
+                Console.WriteLine();
+            }
+            // empty Rhombus
+            for (int i = 0; i < N; i = Math.Abs(i) + 1)
+            {
+                for (int j = 1; j <= 2 * N - 1; j++)
+                {
+                    Console.Write((j == N - i || j == N + i) ? visible : unvisible);
+                }
+                Console.WriteLine();
+            }
+            for (int p = 1; p <= N - 1; p++)
+            {
+                for (int k = 1; k <= 2 * N - 1; k++)
+                {
+                    Console.Write((k == p + 1 || k == 2 * N - p - 1) ? visible : unvisible);
+                }
+                Console.WriteLine();
+            }
+            Console.
             Console.ReadKey();
-    } 
+
+        }
     }
 }
