abe44e0e0b7664716c0b529b19c54550105b4159
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-14 14:31:09 +0500

    add Task 22

---
+++ study-C-sharp/Task 22/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Խնդիր_22:
+Հաշվել տրված թվի բոլոր բաժանարարների գումարը։
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("enter any number ");
+            long num = Convert.ToInt64(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine(i);
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine("bajanararneri gumar@ = "+sum);
+            Console.ReadKey();
+        }
+    }
+}
