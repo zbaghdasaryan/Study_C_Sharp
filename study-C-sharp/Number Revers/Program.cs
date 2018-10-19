b403e87ad77e5c452842488d61c46ed4a5965fbf
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-19 10:31:03 +0400

    add number reverse

---
+++ study-C-sharp/Number Revers/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Խնդիր_27:
+Ներածել n բնական թիվը և տպել այդ թիվը՝ շրջելով և նրա ամեն մի թվանշանից հետո ավելացնելով 0 
+(օրինակ՝ 125 - 502010):
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number_Reverse
+{
+    class Program
+    {
+        static void reversNumber(int num)
+        {
+            while (num > 0)
+            {
+                int digit = num % 10;
+                num /= 10;
+                Console.Write(digit+"0");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            reversNumber(125);
+            Console.ReadKey();
+        }
+    }
+}
