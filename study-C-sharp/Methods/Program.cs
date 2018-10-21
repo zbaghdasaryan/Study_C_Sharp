da80cc1a3cd4f7220e7025f662c00915330d74f7
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-21 21:07:55 +0500

    add Methods

---
+++ study-C-sharp/Methods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Calculator(double num1, double num2)
+        {
+            double sum = num1 + num2;
+            Console.WriteLine("firstNumber + secondNumber = "+sum);
+            double diff = num1 - num2;
+            Console.WriteLine("firstNumber - secondNumber = "+diff);
+            double mult = num1 * num2;
+            Console.WriteLine("firstNumber * secondNumber = "+mult);
+            if (num2 != 0)
+            {
+                double div = num1 / num2;
+                Console.WriteLine("firstNumber / secondNumber = "+div);
+            }
+            else
+            {
+                Console.WriteLine("can't divide to 0, please choose another secondNumber");
+            }
+        }
+        static void ArithmaticalGeometricalMeans (double num1, double num2)
+        {
+            double am = (num1 + num2) / 2;
+            Console.WriteLine("arithmetic mean is equal " + am);
+            double gm = Math.Sqrt(num2 * num2);
+            Console.WriteLine("geometric mean is equal " + gm);
+        }
+        static double GreatestCommonDivisor(double num1, double num2)
+        {
+            double minnumber = Math.Min(num1, num2);
+
+            for (double i = minnumber; i >= 2; i--)
+            {
+                if (num1 % i == 0 & num2 % i == 0)
+                    return i;
+            }
+
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the first number ");
+            double firstNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("please enter the second number ");
+            double secondNumber = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(GreatestCommonDivisor(firstNumber, secondNumber));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ study-C-sharp/palindrome string/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polindrome
+{
+    class Program
+    {
+  static bool PolindromeString(string text)
+        {
+            if (text.Length == 1)
+                return true;
+            else
+            {
+                for (int i = 0; i <= text.Length / 2; i++)
+                {
+                    if (text[i] == text[text.Length - i - 1])
+                        continue;               
+                else
+                        return false;
+                }
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            string text = Convert.ToString(Console.ReadLine());
+            if (PolindromeString(text)==true)
+                Console.WriteLine("polindrome");
+            else
+                Console.WriteLine("NOT polindrome");
+            Console.ReadKey();
+        }                    
+    }
+}
+
