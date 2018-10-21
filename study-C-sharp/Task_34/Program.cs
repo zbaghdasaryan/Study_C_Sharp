e676ec4cb96ca5353ede77ad9be1e9570c950a9d
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-10-21 15:39:45 +0500

    add Task 34

---
+++ study-C-sharp/Task_34/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Խնդիր_34:
+Տրված են X իրական և N բնական թվերը։ 
+Գտնել X – X3/(3!) + X5/(5!) – … + (–1)N·X2·N+1/((2·N+1)!) արտահայտության արժեքը (N! = 1·2·…·N)։ 
+Ստացված թիվը sin(X)-ի մոտավոր արժեքն է։
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter any natural number");
+            int N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter any real number");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double sum = x;      
+            for (int i = 1; i <= N; i++)
+            {
+                sum += 1.0 * Math.Pow(-1, i)*Math.Pow(x,2*i+1) / (2*i*(2*i+1));               
+            }
+            Console.WriteLine(sum); 
+            Console.ReadKey();
+        }
+    }
+}
