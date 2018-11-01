e2bd8452720ca9c1a1bbc7fac4e9d6174ae6530a
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-01 16:43:54 +0400

    add Class Jet

---
+++ study C sharp second stage/business tour/Aeroport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business_tour
+{
+    public class Aeroport
+    {
+        public string NameOfAeroport { get; set; }
+        public Jet[] jetArry { get; set ; }
+        int i;
+
+
+        public Aeroport(string nameAeroport, int N)
+        {
+            NameOfAeroport = nameAeroport;
+            jetArry = new Jet[N];
+        }
+
+        public void FillAeroport(Jet aeroplane)
+        {
+            jetArry[i++] = aeroplane;
+        }
+
+
+
+
+
+
+    }     
+}
---
+++ study C sharp second stage/business tour/Jet.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business_tour
+{
+    public class Jet
+    {
+        public string JetName { get ; set ; }
+        public int JetDistance { get; set; }
+        public int JetPlaces { get ; set ; }
+        public int JetSpeed { get ; set ; }
+        public string JetColor { get ; set ; }
+
+
+        public Jet()
+        {
+
+        }
+
+        public Jet(string name, int distance, int places, int speed, string color)
+        {
+            JetName = name;
+            JetDistance = distance;
+            JetPlaces = places;
+            JetSpeed = speed;
+            JetColor = color;
+        }
+
+       
+
+        public void PrintJet()
+        {
+            Console.WriteLine("jet name is {0}\n jet fly dictanc {1} km\n jet places is {2}\n jet max speed is {3}\n jet color is {4}\n", JetName, JetDistance, JetPlaces, JetSpeed, JetColor);
+
+        }
+
+
+
+
+        //int[] jetarray = new int [5]
+
+    }
+}
