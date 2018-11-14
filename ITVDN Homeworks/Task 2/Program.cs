fda892300c0258dfa5cc26b1ba620596807276fe
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-14 12:12:51 +0400

    Rectangle

---
+++ ITVDN Homeworks/Task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("enter the first side of rectangle side1 = ");
+            //Считывание значений из стандартного входного потока в переменную side
+            double side1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("enter the second side of rectangl side2 = ");
+            //Считывание значений из стандартного входного потока в переменную side2
+            double side2 = Convert.ToDouble(Console.ReadLine());
+
+            //Создание экземпляра класса и передача в пользовательский конструктор двух аргументов
+            Rectangle rectangle = new Rectangle(side1, side2);
+            //Отображение периметра и площади прямоугольника
+            Console.WriteLine("Perimeter = {0}, Area= {1}", rectangle.Perimeter, rectangle.Area);
+           
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ITVDN Homeworks/Task 2/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Rectangle
+    {
+        double side1, side2;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public double AreaCalculator()
+        {
+            return side1 * side2;
+        }
+        public double PerimeterCalculator()
+        {
+            return 2 * (side1 + side2);
+        }
+
+
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+
+        }
+    }
+}
