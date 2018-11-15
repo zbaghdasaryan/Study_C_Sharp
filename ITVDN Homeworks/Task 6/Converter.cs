eb4756ea3ddc24dc7eb4cfd5631dce5775214a82
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-15 14:41:33 +0400

    Task 6

---
+++ ITVDN Homeworks/Task 6/Converter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class Converter
+    {
+        double arq_usd, arq_eur, arq_rub, vach_usd, vach_eur, vach_rub;
+        public Converter(double arq_usd, double vach_usd, double arq_eur, double vach_eur, double arq_rub, double vach_rub)
+        {
+            this.arq_usd = arq_usd;
+            this.vach_usd = vach_usd;
+
+            this.arq_eur = arq_eur;
+            this.vach_eur = vach_eur;
+
+            this.arq_rub = arq_rub;
+            this.vach_rub = vach_rub;
+        }
+
+        public void ToUsd(double dram)
+        {
+            Console.WriteLine("dzer mutqagrac dram@ hamarjeq e {0} dollari", dram / vach_usd);
+        }
+        public void FromUsd(double usd)
+        {
+            Console.WriteLine("dzer mutqagrac dollar@ hamarjeq e {0} drami", usd * arq_usd);
+        }
+        public void ToEur(double dram)
+        {
+            Console.WriteLine("dzer mutqagrac dram@ hamarjeq e {0} evroi", dram / vach_eur);
+        }
+        public void FromEur(double eur)
+        {
+            Console.WriteLine("dzer mutqagrac evron hamarjeq e {0} drami", eur * arq_eur);
+        }
+        public void ToRub(double dram)
+        {
+            Console.WriteLine("dzer mutqagrac dram@ hamarjeq e {0} rublu", dram / vach_rub);
+        }
+        public void FromRub(double rub)
+        {
+            Console.WriteLine("dzer mutqagrac rublin hamarjeq e {0} drami", rub * arq_rub);
+        }
+    }
+}
---
+++ ITVDN Homeworks/Task 6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter converter = new Converter(485, 486.5, 548, 552, 7.25, 7.33);
+
+            converter.ToUsd(486500);
+            converter.FromUsd(100);
+            converter.FromRub(1000);
+            converter.ToEur(500000);
+            Console.ReadKey();
+        }
+    }
+}
