d65c4a6681c24f096a98796158a482acd251058b
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-22 14:46:57 +0400

    Task 11

---
+++ ITVDN Homeworks/Task 11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ship ship = new Ship(2000000, 250, 2018) { Places = 20, NamePort = "Sevan" };
+
+
+            Console.WriteLine("vehical price is {0}, max speed is {1}, Year {2}, places {3}, vehical now is in port {4}",
+                                ship.Price, ship.MaxSpeed, ship.Year, ship.Places, ship.NamePort);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ITVDN Homeworks/Task 11/Vehicle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_11
+{
+    class Vehicle
+    {
+
+        public uint Price { get; set; }
+        public uint MaxSpeed { get; set; }
+        public uint Year { get; set; }
+        public Vehicle(uint Price, uint MaxSpeed, uint Year)
+        {
+            this.Price = Price;
+            this.MaxSpeed = MaxSpeed;
+            this.Year = Year;
+        }
+    }
+    class Car : Vehicle
+    {
+        public Car(uint Price, uint MaxSpeed, uint Year) : base(Price, MaxSpeed, Year)
+        {
+        }
+    }
+    class Ship : Vehicle
+    {
+
+        public Ship(uint Price, uint MaxSpeed, uint Year) : base(Price, MaxSpeed, Year)
+        {
+
+        }
+        private uint places;
+        public uint Places
+        {
+            get { return places; }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("places must be >0");
+                }
+                else
+                {
+                    places = value;
+                }
+            }
+        }
+        private string namePort;
+        public string NamePort
+        {
+            get { return namePort; }
+            set
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("please enter name of port");
+                }
+                else
+                {
+                    namePort = value;
+                }
+            }
+        }
+    }
+    class Plane : Vehicle
+    {
+        public Plane(uint Price, uint MaxSpeed, uint Year) : base(Price, MaxSpeed, Year)
+        {
+        }
+
+        public int Hight { get; set; }
+
+        public uint Places
+        {
+            get { return Places; }
+            set
+            {
+                if (Places < 0)
+                {
+                    Console.WriteLine("places must be >0");
+                }
+                value = Places;
+            }
+        }
+    }
+}
