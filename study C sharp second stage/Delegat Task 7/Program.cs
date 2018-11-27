fe8ec11e2f208ced3517c8e0ed5ca76d9fc09402
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-27 15:29:52 +0400

    Delegat Task 7

---
+++ study C sharp second stage/Delegat Task 7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Анонимные (лямбда) методы.
+
+namespace Delegates
+{
+    // Создаем класс делегата.
+    public delegate void MyDelegate(ref int a, ref int b, out int c);
+
+    class Program
+    {
+        static void Main()
+        {
+            int summand1 = 1, summand2 = 2, sum;
+
+            MyDelegate myDelegate = delegate (ref int a, ref int b, out int c) { a++; b++; c = a + b; };
+
+            myDelegate(ref summand1, ref summand2, out sum);
+
+            Console.WriteLine("{0} + {1} = {2}", summand1, summand2, sum);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
