82f3a6b5a5c1f579ecaae34a695adf7054a164b6
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-27 12:14:59 +0400

    Delegat Task 2

---
+++ study C sharp second stage/Delegat Task 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+// Делегаты.
+
+namespace Delegates
+{
+    // Класс, метод которого будет сообщен с делегатом.
+    class MyClass
+    {
+        // Создаем метод, который планируем сообщить с делегатом.
+        public void Method()
+        {
+            Console.WriteLine("Строку вывел метод сообщенный с делегатом.");
+        }
+    }
+
+    // На 21-й строке создаем класс-делегата с именем MyDelegate,
+    // метод, который будет сообщен с экземпляром данного класса-делегата, 
+    // не будет ничего принимать и не будет ничего возвращать.
+
+    public delegate void MyDelegate();  // Создаем класс делегата. (1) 
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass instance = new MyClass();
+
+            MyDelegate myDelegate = new MyDelegate(instance.Method); // Создаем экземпляр делегата. (2)
+
+            myDelegate.Invoke();   // Вызываем метод сообщенный с делегатом. (3)
+
+            myDelegate();          // Другой способ вызова метода сообщенного с делегатом. (3')
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
