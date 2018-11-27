034eacadc685254de57c804531b4f4f5b50c5ca1
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-27 12:16:25 +0400

    Delegat Task 3

---
+++ study C sharp second stage/Delegat Task 3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+// Делегаты.
+
+namespace Delegates
+{
+    // Класс, метод которого будет сообщен с делегатом.
+    class MyClass
+    {
+        // Создаем метод, который планируем сообщить с делегатом.
+        public string Method(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+
+    // На 21-й строке создаем класс-делегата с именем MyDelegate,
+    // метод, который будет сообщен с экземпляром данного класса-делегата, 
+    // будет принимать один строковой аргумент и возвращать строковое значение.
+
+    public delegate string MyDelegate(string name);  // Создаем класс делегата. (1) 
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass instance = new MyClass();
+
+            MyDelegate myDelegate = new MyDelegate(instance.Method); // Создаем экземпляр делегата и сообщаем с ним метод. (2)
+
+            string greeting = myDelegate.Invoke("Jeffrey Richter"); // Вызываем метод сообщенный с делегатом. (3)
+
+            Console.WriteLine(greeting);
+
+            greeting = myDelegate("Grady Booch"); // Другой способ вызова метода сообщенного с делегатом. (3')
+
+            Console.WriteLine(greeting);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ study C sharp second stage/Delegat Task 3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Delegat Task 3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Delegat Task 3")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4b32fbf2-d226-438a-a9c7-d24a9b2f182f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
