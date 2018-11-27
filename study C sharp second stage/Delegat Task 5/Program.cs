9ea8cb0d616045b3eb380289fab441d96bd64a9f
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-27 15:23:44 +0400

    Delegat Task 6

---
+++ study C sharp second stage/Delegat Task 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Анонимные (лямбда) методы.
+
+namespace Delegates
+{
+    // Создаем класс делегата.
+    public delegate void MyDelegate();
+
+    class Program
+    {
+        static void Main()
+        {
+            // Создаем экземпляр класса-делегата MyDelegate и сообщаем с ним анонимный метод.
+            MyDelegate myDelegate = delegate { Console.WriteLine("Hello world!"); };
+
+            // Вызов анонимного метода, сообщенного с делегатом.
+            myDelegate();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ study C sharp second stage/Delegat Task 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Анонимные (лямбда) методы.
+
+namespace Delegates
+{
+    // Создаем класс делегата.
+    public delegate int MyDelegate(int a, int b);
+
+    class Program
+    {
+        static void Main()
+        {
+            int summand1 = 1, summand2 = 2, sum = 0;
+
+            MyDelegate myDelegate = delegate (int a, int b) { return a + b; };
+
+            sum = myDelegate(summand1, summand2);
+
+            Console.WriteLine("{0} + {1} = {2}", summand1, summand2, sum);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
