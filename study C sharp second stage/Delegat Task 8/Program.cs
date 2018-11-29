1a7c85bf00b5b867431b4a68f4666d79b559f206
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-29 10:51:49 +0400

    Delegat Task 8

---
+++ study C sharp second stage/Delegat Task 8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Лямбда выражения и лямбда операторы.
+
+namespace Delegates
+{
+    public delegate int MyDelegate(int a);
+
+    class Program
+    {
+        static void Main()
+        {
+            MyDelegate myDelegate;
+
+            myDelegate = delegate (int x) { return x * 2; }; // Лямбда-Метод 
+
+            myDelegate = (x) => { return x * 2; };          // Лямбда-Оператор.
+
+            myDelegate = x => x * 2;                        // Лямбда-Выражение.
+
+            int result = myDelegate(4);
+            Console.WriteLine(result);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
