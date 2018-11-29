7aeae569ffe5ba2dd671dfaad17c709e4179784b
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-29 11:36:53 +0400

    Delegat task 10

---
+++ study C sharp second stage/Delegat Task 10/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Делегаты.
+
+namespace Delegates
+{
+    delegate Delegate2 Delegate1();  // 1.
+    delegate void Delegate2();       // 2.
+
+
+    class Program
+    {
+        public static Delegate2 Method1()     // 1.
+        {
+            return new Delegate2(Method2);
+        }
+
+        public static void Method2()          // 2.
+        {
+            Console.WriteLine("Hello world!");
+        }
+
+        static void Main()
+        {
+            Delegate1 delegate1 = new Delegate1(Method1);
+
+            Delegate2 delegate2 = delegate1();
+
+            delegate2();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
