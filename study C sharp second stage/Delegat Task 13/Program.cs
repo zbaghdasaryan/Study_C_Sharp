7942d0c54cff15e8ac6c88a787e397647b70766b
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-29 13:23:00 +0400

    Delegat Task 13

---
+++ study C sharp second stage/Delegat Task 13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+// Делегаты. (Предположение делегата)
+
+namespace Delegates
+{
+    static class MyClass
+    {
+        public static void Method()
+        {
+            Console.WriteLine("Строку вывел метод сообщенный с делегатом.");
+        }
+    }
+
+    public delegate void MyDelegate();
+
+    class Program
+    {
+        static void Main()
+        {
+            // MyDelegate myDelegate = new MyDelegate(MyClass.Method);
+
+            MyDelegate myDelegate = MyClass.Method; // Предположение делегата.
+            myDelegate();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
