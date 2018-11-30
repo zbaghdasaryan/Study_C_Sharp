c1569340eefd77a3a532220791b0a15a77ba3a42
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-11-30 14:09:26 +0400

    Delegat Task 1

---
+++ ITVDN Homeworks/Delegat Task 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegat_Task_1
+{
+    class Program
+    {
+        public delegate double DelegatArithmeticMean(double a, double b, double c);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter the second number");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("enter the third number");
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            double del(double a, double b, double c) => (double)(a + b + c) / 3;
+
+            Console.WriteLine(del(x,y,z));
+
+            Console.ReadKey();
+        }
+    }
+}
