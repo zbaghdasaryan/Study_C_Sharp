f6da8ca8c138c2d79756453a37d8d71a0f9b09cb
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-03 14:54:40 +0400

    Calculator try catch

---
+++ study C sharp second stage/Calculator try catch/Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_try_catch
+{
+    class Calculator
+    {
+        public double Add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public double Sub(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        public double Mult(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        public double? Div(double firstNumber, double secondNumber)
+        {
+
+            try
+            {
+                return firstNumber / secondNumber;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ study C sharp second stage/Calculator try catch/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator_try_catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter the first number ");
+            double firstNumber = 0;
+            try
+            {
+                firstNumber = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.GetType());
+            }
+
+            Console.WriteLine("enter the second number ");
+            double secondNumber = 0;
+            try
+
+            {
+                secondNumber = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+                
+            }
+
+            Console.WriteLine("enter mat operator ");
+            char op = '0';
+            try
+            {
+                op = char.Parse(Console.ReadLine());
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Calculator calculator = new Calculator();
+                double? result = 0;
+                bool calculation = true;
+
+                switch (op)
+                {
+                    case '+':
+                        result = calculator.Add(firstNumber, secondNumber);
+                        break;
+                    case '-':
+                        result = calculator.Sub(firstNumber, secondNumber);
+                        break;
+                    case '*':
+                        result = calculator.Mult(firstNumber, secondNumber);
+                        break;
+                    case '/':
+                        result = calculator.Div(firstNumber, secondNumber);
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("wrong operator");
+                            calculation = false;
+                            break;
+                        }
+
+                }
+                if (calculation && result != null)
+                {
+                    Console.WriteLine("{0} {1} {2} = {3}", firstNumber, op, secondNumber, result);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
