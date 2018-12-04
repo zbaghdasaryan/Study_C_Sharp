33f7f6b3b8f35d6c371252c08b1a4a83d8737369
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-04 14:26:41 +0400

    Generic Task 4

---
+++ study C sharp second stage/Generic Task 4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+// Универсальные шаблоны. (Универсальный делегат)
+
+namespace Generics
+{
+    // На 11-й строке создаем класс-делегата с именем  MyDelegate, параметризированный двумя Указателями Места Заполнения Типом - Т и R,
+    // метод который будет сообщен с экземпляром данного класса-делегата, будет принимать один аргумент, типа Указателя Места Заполнения Типом - Т,
+    // и возвращать значение типа Указателя Места Заполнения Типом - R.
+
+    delegate R MyDelegate<T, R>(T t);
+
+    class Program
+    {
+        public static int Add(int i)
+        {
+            return ++i;
+        }
+
+        public static string Concatenation(string s)
+        {
+            return "Hello " + s + "!";
+        }
+
+
+        static void Main()
+        {
+            MyDelegate<int, int> myDelegate1 = new MyDelegate<int, int>(Add);
+            int i = myDelegate1.Invoke(1);
+           
+            Console.WriteLine(i);
+
+            MyDelegate<string, string> myDelegate2 = new MyDelegate<string, string>(Concatenation);
+            string s = myDelegate2("Alex");
+            Console.WriteLine(s);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
