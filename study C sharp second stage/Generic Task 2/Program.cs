4b8c6e0eed141ff72c1ddd14ea58185d878ee1c5
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-04 12:40:36 +0400

    change Generic Task 2

--- study C sharp second stage/Generic Task 2/Program.cs
+++ study C sharp second stage/Generic Task 2/Program.cs
@@ -4,17 +4,19 @@
 
 namespace Generics
 {
-    class MyGenerics<TYPE1, TYPE2>
+    class MyGenerics<TYPE1, TYPE2, TYPE3>
     {
         // Поля
         private TYPE1 variable1;
         private TYPE2 variable2;
+        private TYPE3 variable3;
 
         // Конструктор.
-        public MyGenerics(TYPE1 argument1, TYPE2 argument2)
+        public MyGenerics(TYPE1 argument1, TYPE2 argument2, TYPE3 argument3)
         {
             this.variable1 = argument1;
             this.variable2 = argument2;
+            this.variable3 = argument3;
         }
 
         // Свойства.
@@ -29,19 +31,24 @@ namespace Generics
             get { return this.variable2; }
             set { this.variable2 = value; }
         }
+
+        public TYPE3 Variable3
+        {
+            get { return this.variable3;}
+            set {this.variable3 = value; }
     }
 
     class Program
     {
         static void Main()
         {
-            MyGenerics<int, int> instance1 = new MyGenerics<int, int>(1, 2);
+            MyGenerics<int, int, int> instance1 = new MyGenerics<int, int, int>(1, 2, 20);
             Console.WriteLine(instance1.Variable1 + instance1.Variable2);
 
-            MyGenerics<string, int> instance2 = new MyGenerics<string, int>("Number ", 1);
+            MyGenerics<string, int, double> instance2 = new MyGenerics<string, int, double>("Number ", 1, 20.1);
             Console.WriteLine(instance2.Variable1 + instance2.Variable2);
 
-            MyGenerics<string, string> instance3 = new MyGenerics<string, string>("Hello ", "World");
+            MyGenerics<string, string, byte> instance3 = new MyGenerics<string, string, byte>("Hello ", "World",8);
             Console.WriteLine(instance3.Variable1 + instance3.Variable2);
 
             // Delay.
