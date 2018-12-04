002c3743be19c33a13a1d3677c602785829fdd09
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-04 12:53:22 +0400

    Generic Task 3

---
+++ study C sharp second stage/Generic Task 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// Универсальные шаблоны. (Универсальный метод)
+
+namespace Generics
+{
+    class MyClass
+    {
+        public void Method<T>(T argument)
+        {
+            T variable = argument;
+
+            Console.WriteLine(variable);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass instance1 = new MyClass();
+            MyClass instance2 = new MyClass();
+
+            instance1.Method<string>("Hello world!");
+
+            instance1.Method("hello!");
+            instance2.Method(12);
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
