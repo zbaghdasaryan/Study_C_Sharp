707bbdb9b11892a0cdc147a1fba9e10ad973e009
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-04 12:28:25 +0400

    Generic Task 2

---
+++ study C sharp second stage/Generic Task 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+// Универсальные шаблоны.
+
+namespace Generics
+{
+    class MyGenerics<TYPE1, TYPE2>
+    {
+        // Поля
+        private TYPE1 variable1;
+        private TYPE2 variable2;
+
+        // Конструктор.
+        public MyGenerics(TYPE1 argument1, TYPE2 argument2)
+        {
+            this.variable1 = argument1;
+            this.variable2 = argument2;
+        }
+
+        // Свойства.
+        public TYPE1 Variable1
+        {
+            get { return this.variable1; }
+            set { this.variable1 = value; }
+        }
+
+        public TYPE2 Variable2
+        {
+            get { return this.variable2; }
+            set { this.variable2 = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyGenerics<int, int> instance1 = new MyGenerics<int, int>(1, 2);
+            Console.WriteLine(instance1.Variable1 + instance1.Variable2);
+
+            MyGenerics<string, int> instance2 = new MyGenerics<string, int>("Number ", 1);
+            Console.WriteLine(instance2.Variable1 + instance2.Variable2);
+
+            MyGenerics<string, string> instance3 = new MyGenerics<string, string>("Hello ", "World");
+            Console.WriteLine(instance3.Variable1 + instance3.Variable2);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
