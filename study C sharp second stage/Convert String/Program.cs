4d39343c03b71d7fd6e4608c05d7aec74b03c0df
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-05 16:28:36 +0400

    Convert from String

---
+++ study C sharp second stage/Convert String/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Convert_String
+{
+    static class Convert<T> where T : struct
+    {
+        public static T? Get(string text)
+        {
+            T? ret = null;
+            try
+            {
+                ret = (T)Convert.ChangeType(text, typeof(T));
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("wrong type");
+                Console.WriteLine(e.Message);
+            }
+            return ret;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the text ");
+            string text = Console.ReadLine();
+            char? newType = Convert<char>.Get(text);
+            Console.WriteLine(newType);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ study C sharp second stage/Convert String/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Convert String")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Convert String")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("48d18b05-0d08-4150-b5e6-c7b5f6f28861")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
