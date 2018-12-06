304b7fc12cb6e9026d10ec8934e70f4b5d40cbfc
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-06 13:11:59 +0400

    Anonymous Task 5

---
+++ study C sharp second stage/Anonymous Task 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+// Анонимные типы.
+
+namespace Anonymous
+{
+    delegate void MyDelegate(string @string);
+
+    class Program
+    {
+        static void Main()
+        {
+            var instance = new
+            {
+                MyDel = new MyDelegate((string @string) => Console.WriteLine(@string))
+            };
+
+            instance.MyDel("Hello world!");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
