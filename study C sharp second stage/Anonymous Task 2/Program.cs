dff85c01441bb3a41a69d8124c89a97cf86e3afe
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-06 12:38:16 +0400

    Anonymous Task 2

---
+++ study C sharp second stage/Anonymous Task 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+// Анонимные типы.
+
+namespace Anonymous
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Анонимные типы в анонимных типах.
+
+            var instance = new { Name = "Alex", Age = 27, Id = new { Number = 123 } };
+
+            Console.WriteLine("Name = {0}, Age = {1}, Id = {2}", instance.Name, instance.Age, instance.Id.Number);
+
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
