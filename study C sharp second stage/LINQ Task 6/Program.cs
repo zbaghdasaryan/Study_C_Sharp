ea8ce487ced9746eb78c47579b571c931890b187
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-06 16:56:35 +0400

    LINQ Task 6

---
+++ study C sharp second stage/LINQ Task 6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+// Применение запроса к коллекции которая поддерживает IEnumerable не параметризированный.
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main()
+        {
+            ArrayList numbers = new ArrayList();
+            numbers.Add(1);
+            numbers.Add(2);
+
+            // ЯВНОЕ указание типа Int32 переменной диапазона - n.  (var - НЕВОЗМОЖНО использовать т.к. IEnumerable не параметризированный!)
+            var query = from int n in numbers
+                        select n * 2;
+
+            foreach (var item in query)
+                Console.WriteLine(item);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
