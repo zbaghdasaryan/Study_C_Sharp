57a3349f621c22281036e8a6176c460b69399fcd
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-06 12:51:32 +0400

    Anonymous Task 4

---
+++ study C sharp second stage/Anonymous Task 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+// Анонимные типы. (Слабая ссылка)
+
+namespace Anonymous
+{
+    class MyClass
+    {
+        public int field;
+
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            new { My = new MyClass { field = 1 } }.My.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
