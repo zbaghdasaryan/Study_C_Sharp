4918ba5791ea0fcd74b9d4720f71eed80f97b9b7
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-06 16:48:37 +0400

    LINQ Task 5

---
+++ study C sharp second stage/LINQ Task 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// Таблица умножения.
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Конструкция from похожа на оператор foreach.
+            // Использование нескольких конструкций from, аналогично вложенным операторам foreach.
+
+            var query = from x in Enumerable.Range(1, 9) // Таблица умножения от 1 до 9.
+                        from y in Enumerable.Range(1, 10)
+                        select new
+                        {
+                            X = x,
+                            Y = y,
+                            Product = x * y
+                        };
+
+            foreach (var item in query)
+                Console.WriteLine("{0} * {1} = {2}", item.X, item.Y, item.Product);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
