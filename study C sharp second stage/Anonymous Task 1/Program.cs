13243c161559fa8847feb260c0b57583cf659581
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-06 12:14:49 +0400

    Anonymous Task 1

---
+++ study C sharp second stage/Anonymous Task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Анонимные типы.
+
+// Анонимные типы предлагают удобный способ инкапсулирования набора свойств в один объект 
+// без необходимости предварительного явного определения типа.
+// Имя типа создается компилятором и недоступно на уровне исходного кода.
+// Анонимные типы являются ссылочными типами, которые происходят непосредственно от класса object.
+// Компилятор присваивает им имена, несмотря на то что эти имена недоступны для приложения.
+
+namespace Anonymous
+{
+    //class MyClass
+    //{
+    //    public string Name { get; internal set; }
+    //    public int Age { get; internal set; }
+    //}
+
+    class Program
+    {
+        static void Main()
+        {
+            var instance = new { Name = "Zoro", Age = 34 };
+            //instance.Name = "zzz"; only get !
+            Console.WriteLine("Name = {0}, Age = {1}", instance.Name, instance.Age);
+
+            Type type = instance.GetType();
+           
+            Console.WriteLine(type.ToString());
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
