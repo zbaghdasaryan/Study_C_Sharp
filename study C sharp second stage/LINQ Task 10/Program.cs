1f22c491e0651731161b6bb0288c36f597b86ed8
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-08 13:57:02 +0400

    LINQ Task 10

---
+++ study C sharp second stage/LINQ Task 10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// select - (Опреация проекции) используется для производства конечного результата запроса.
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3, 4 };
+
+            // Построить запрос.
+            var query = from x in numbers
+                        select new { Input = x, Output = x * 2 };
+
+            foreach (var item in query)
+                Console.WriteLine("Input = {0}, \t Output = {1}", item.Input, item.Output);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
