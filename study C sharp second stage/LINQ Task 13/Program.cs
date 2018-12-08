d0476b06d3a2a13f5c367596315bdedcdf555c1b
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-08 14:11:42 +0400

    LINQ Task 13

---
+++ study C sharp second stage/LINQ Task 13/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+// group - является средством для разделения ввода запроса.
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Построить запрос.
+            // Разделение чисел на четные и нечетные.
+            var query = from x in numbers
+                        group x by x % 3;
+
+            foreach (var group in query)
+            {
+                Console.WriteLine("mod{0} == {0}", group.Key);
+
+                foreach (var number in group)
+                    Console.WriteLine("{0}, ", number);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
