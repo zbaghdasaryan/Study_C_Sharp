019b0b777f8b4f1fb9223d44d621857bbf09d0a6
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-08 14:16:41 +0400

    LINQ Task 16

---
+++ study C sharp second stage/LINQ Task 16/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+// into - подобно let, позволяет определить локальный по отношению к запросу идентификатор.
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            // Построить запрос.
+            // Разделение чисел на четные и нечетные.
+            var query = from x in numbers
+                        group x by x % 2 into partition
+                        select new { Key = partition.Key, Count = partition.Count(), Group = partition };
+
+
+            foreach (var item in query)
+            {
+                Console.WriteLine("mod2 == {0}", item.Key);
+                Console.WriteLine("Count == {0}", item.Count);
+
+                foreach (var number in item.Group)
+                    Console.Write("{0}, ", number);
+
+                Console.WriteLine("\n");
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
