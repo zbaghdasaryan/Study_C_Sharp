52ef16bc0d3a6f3d3a254d91380aacc7742303ca
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-10 17:33:11 +0400

    change operator matrix

--- study C sharp second stage/Operator Matrix/Program.cs
+++ study C sharp second stage/Operator Matrix/Program.cs
@@ -5,73 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Operator_Matrix
-{
-    class Matrix
-    {
-        int[,] array;
-
-        int rows, collumns;
-
-        public int Rows
-        {
-            get { return rows; }
-            set
-            {
-                if (value > 0)
-                    rows = value;
-            }
-        }
-
-        public int Collumns
-        {
-            get { return collumns; }
-            set
-            {
-                if (value > 0)
-                    collumns = value;
-            }
-        }
-
-        public int this[int i, int j]
-        {
-            get { return array[i, j]; }
-
-            set { array[i, j] = value; }
-        }
-
-        public Matrix(int countOfRows, int countOfCollumns)
-        {
-            Rows = countOfCollumns;
-            Collumns = countOfCollumns;
-            array = new int[Rows, Collumns];
-        }
-
-        public void Show()
-        {
-            for (int i = 0; i < Rows; i++)
-            {
-                for (int j = 0; j < Collumns; j++)
-                {
-                    Console.Write("{0,6}", array[i, j]);
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-        }
-
-        public static Matrix operator *(Matrix a, Matrix b)
-        {
-            Matrix mult = new Matrix(a.Rows, b.Collumns);
-            for (int i = 0; i < a.Rows; i++)
-                for (int k = 0; k < b.Collumns; k++)
-                    for (int j = 0; j < a.Collumns; j++)
-
-                        mult[i, k] += a[i, j] * b[j, k];
-
-            return mult;
-        }
-    }
-
+{         
     class Program
     {
         static void Main(string[] args)
@@ -124,12 +58,15 @@ namespace Operator_Matrix
                     b[i, j] = random.Next(100);
 
             a.Show();
+            Console.ReadLine();
             b.Show();
-            //c.Show();
+            Console.ReadLine();
+           // c.Show();
 
-            // c = a * b;
-            // Console.WriteLine("after mult");
-            // c.Show();
+            c = a * b;
+            Console.WriteLine("after mult\n");
+            c.Show();
+            Console.ReadKey();
         }
     }
 }
