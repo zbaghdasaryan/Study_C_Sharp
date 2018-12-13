b665a9bd4982929a7253952ca62f5bcdade50a6e
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-13 11:53:59 +0400

    Abstract Task 1

---
+++ study C sharp second stage/Abstract Class Task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+// Абстрактный класс.
+
+namespace Abstraction
+{
+    // Абстрактный класс.
+    abstract class AbstractClass
+    {
+        public abstract void Method();
+    }
+
+    // Конкретный класс.
+    class ConcreteClass : AbstractClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("Implementation");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            AbstractClass instance = new ConcreteClass();
+
+            instance.Method();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
