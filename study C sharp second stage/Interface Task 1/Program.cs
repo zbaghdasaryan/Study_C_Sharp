a39c6c500260d8b1626cabe5c6447fc082f91d44
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-13 12:40:52 +0400

    Interface Task 1

---
+++ study C sharp second stage/Interface Task 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+// Интерфейсы.
+
+namespace Interfaces
+{
+    interface IInterface
+    {
+        void Method();
+    }
+
+    class MyClass : IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Метод - реализация Интерфейса.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyClass my = new MyClass();
+
+            my.Method();
+
+            //Delay.
+            Console.ReadKey();
+        }
+    }
+}
