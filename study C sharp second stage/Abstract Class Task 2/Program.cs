effae506fa81834ba59eea742bc192fd0b24a226
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-13 11:55:02 +0400

    Abstract Task 2

---
+++ study C sharp second stage/Abstract Class Task 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+// Абстрактный класс может быть унаследован от конкретного класса.
+
+namespace Abstraction
+{
+    // Конкретный класс A.
+    class ConcreteClassA
+    {
+        public void Operation()
+        {
+            Console.WriteLine("ConcreteClassA.Operation");
+        }
+    }
+
+    // Абстрактный класс.
+    abstract class AbstractClass : ConcreteClassA
+    {
+        public abstract void Method();
+    }
+
+    // Конкретный класс B.
+    class ConcreteClassB : AbstractClass
+    {
+        public override void Method()
+        {
+            Console.WriteLine("ConcreteClassB.Method");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            AbstractClass instance = new ConcreteClassB();
+
+            instance.Method();
+            instance.Operation();
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
