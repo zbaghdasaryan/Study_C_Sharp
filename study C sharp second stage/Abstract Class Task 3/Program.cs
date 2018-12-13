b5cca12704d4ae8c03a3f60a9b086f956020a8b9
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-13 11:57:14 +0400

    Abstract Task 3

---
+++ study C sharp second stage/Abstract Class Task 3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+// Абстрактный класс может быть унаследован от абстрактного класса.
+// Реализация абстрактного метода из базового абстрактного класса, в производном абстрактном классе - не обязательна.
+
+namespace Abstraction
+{
+    // Абстрактный класс A.
+    abstract class AbstractClassA
+    {
+        public abstract void OperationA();
+    }
+
+    // Абстрактный класс B.
+    abstract class AbstractClassB : AbstractClassA
+    {
+        public abstract void OperationB();
+    }
+
+    // Конкретный класс.
+    class ConcreteClass : AbstractClassB
+    {
+        public override void OperationA()
+        {
+            Console.WriteLine("ConcreteClass.OperationA");
+        }
+
+        public override void OperationB()
+        {
+            Console.WriteLine("ConcreteClass.OperationB");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            AbstractClassA instance = new ConcreteClass();
+
+            instance.OperationA();
+
+            //instance.OperationB();  // Вопрос: почему недоступен данный метод?
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
