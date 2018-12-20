3bf9e479a2f2ee94322ebdbcb8f098adb87662c8
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-20 22:31:49 +0400

    ArrayList

---
+++ study C shatp third stage/ArrayList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Array_List
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList info = new ArrayList();
+            string temp;
+            do
+            {
+                Console.WriteLine("any information?");
+                if ((temp = Console.ReadLine()) != "") 
+                info.Add(temp);
+            }
+            while (temp != "");
+            foreach (var item in info)
+            {
+                Console.WriteLine("information is ---> {0}", item);                
+            }
+            Console.ReadKey();
+        }
+    }
+}
