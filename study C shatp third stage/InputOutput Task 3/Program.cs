1a235c16de92c32b339e44aadf48afbef3a2f975
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-21 12:39:23 +0400

    InputOutput Task 3

---
+++ study C shatp third stage/InputOutput Task 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+// Создание директорий.
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main()
+        {
+            var directory = new DirectoryInfo(@".");
+            Console.WriteLine(directory.FullName);
+            // Создание в TESTDIR новых подкаталогов.
+            if (directory.Exists)
+            {
+                // Создаем D:\TESTDIR\SUBDIR.
+                directory.CreateSubdirectory("SUBDIR");
+
+                // Создаем D:\TESTDIR\MyDir\SubMyDir.
+                directory.CreateSubdirectory(@"MyDir\SubMyDir");
+
+                Console.WriteLine("Директории созданы.");
+            }
+            else
+            {
+                Console.WriteLine("Директория с именем: {0}  не существует.", directory.FullName);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
