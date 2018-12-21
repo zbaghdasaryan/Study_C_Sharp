91d70d70471a9b2187d2501c3a87b6a979212573
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-21 13:33:48 +0400

    InputOutput Task 9

---
+++ study C shatp third stage/InputOutput Task 9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+// Чтение/запись файлов.
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Создаем файл.
+            var file = new FileInfo("Text.txt");
+
+            // С помощью экземпляра StreamWriter записываем в файл несколько строк текста. 
+            StreamWriter writer = file.CreateText();
+
+            writer.WriteLine("Первая строка текста...");
+            writer.WriteLine("Вторая строка текста...");
+
+            // Вставляем символ начала новой строки.
+            writer.Write(writer.NewLine);
+
+            writer.WriteLine("Третья строка чисел:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                writer.Write(i + " ");
+            }
+
+            // Вставляем символ начала новой строки.
+            writer.Write(writer.NewLine);
+
+            // Метод Close() автоматически очищает все буферы!
+            writer.Close();
+
+            Console.WriteLine("Файл Text.txt создан и в него записан текст.");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
