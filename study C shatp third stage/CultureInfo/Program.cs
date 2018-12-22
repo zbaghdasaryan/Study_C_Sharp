9c1eef3b98012ec31a642818a171f2af1287c094
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-22 20:17:41 +0400

    CultureInfo

---
+++ study C shatp third stage/CultureInfo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace StringBasics
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Получение информации о текущей культуре.
+            CultureInfo currentCulture = CultureInfo.CurrentCulture;
+            Console.WriteLine("Текущая локаль: {0}.", currentCulture);
+            Console.ReadKey();
+            // Информация о всех культурах доступных в системе.
+            CultureInfo[] cultureInfo = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            Console.WriteLine("В системе определены {0} различные культуры.",
+                cultureInfo.Length);
+
+            foreach (CultureInfo ci in cultureInfo)
+            {
+                Console.WriteLine(ci.EnglishName + " | " + ci.ToString());
+            }
+            // Все культуры созданные пользователем.
+            cultureInfo = CultureInfo.GetCultures(CultureTypes.UserCustomCulture);
+
+            if (cultureInfo.Length < 1)
+            {
+                Console.WriteLine("Пользователь не создавал собственные локали.");
+            }
+            else
+            {
+                Console.WriteLine("Локали, созданные пользователем:");
+
+                foreach (CultureInfo ci in cultureInfo)
+                {
+                    Console.WriteLine(ci.EnglishName + " | " + ci.ToString());
+                }
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
