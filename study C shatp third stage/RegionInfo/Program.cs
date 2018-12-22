7298b84125c5bd56db41f4ceb6153677db5cb844
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-22 20:47:31 +0400

    StringBasics

---
+++ study C shatp third stage/RegionInfo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.IO.Ports;
+
+// Работа с регионом.
+// Список обозначений культур:
+// http://msdn.microsoft.com/en-us/library/system.globalization.cultureinfo(v=vs.71).aspx
+
+namespace StringBasics
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Получение информации о текущем регионе.
+            RegionInfo regionInfo = RegionInfo.CurrentRegion;
+            Console.WriteLine("EnglishName the region:\t{0}.", regionInfo.EnglishName);
+            Console.WriteLine("NativeName the region:\t{0}.", regionInfo.NativeName);
+           
+
+            Console.WriteLine(new string('-', 35));
+
+            Console.WriteLine("CurrencySymbol the region:\t{0}", regionInfo.CurrencySymbol);
+            Console.WriteLine("CurrencyEnglishName:\t{0}", regionInfo.CurrencyEnglishName);
+            Console.WriteLine("CurrencyNativeName:\t{0}", regionInfo.CurrencyNativeName);
+
+            Console.WriteLine(new string('-', 35));
+
+            // Получение информации о текущем формате даты: названия дней.
+            string[] days = CultureInfo.CurrentCulture.DateTimeFormat.DayNames;
+
+            Console.WriteLine("Days of the week:");
+            foreach (string day in days)
+            {
+                Console.WriteLine(day);
+            }
+
+            Console.WriteLine(new string('-', 35));
+
+            // Получение информации о формате даты в немецком языке: названия дней.
+            days = CultureInfo.GetCultureInfo("de-DE").DateTimeFormat.DayNames;
+
+            Console.WriteLine("Days of the week in German:");
+            foreach (string day in days)
+            {
+                Console.WriteLine(day);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
---
+++ study C shatp third stage/StringMoney/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+// Вывод строк с использованием настроек культуры.
+
+namespace StringBasics
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Сумма, которую необходимо вывести на экран.
+            double money = 122343.45;
+
+            // Три доступные культуры.
+            var american = new CultureInfo("en-US");
+            var germany = new CultureInfo("de-DE");
+            var russian = new CultureInfo("ru-RU");
+
+            // Форматируем строки под нужную культуру и сохраняем результат в переменной.
+            string localMoney = money.ToString("C", american);
+            string result = String.Format("Деньги США: {0}", localMoney);
+
+            localMoney = money.ToString("C", germany);
+            result += String.Format("\nДеньги Германии: {0}", localMoney);
+
+            localMoney = money.ToString("C", russian);
+            result += String.Format("\nДеньги России: {0}", localMoney);
+
+            // Выводим содержимое на экран.
+            MessageBox.Show(result);
+        }
+    }
+}
