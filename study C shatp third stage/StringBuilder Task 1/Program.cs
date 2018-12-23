0f990464a5c3cb6d804815959efad185f576874d
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-23 15:53:35 +0400

    StringBuilder

---
+++ study C shatp third stage/StringBuilder Task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+// Использование StringBuilder.
+
+namespace StringBasics
+{
+    public class EntryPoint
+    {
+        static void Main()
+        {
+            var builder = new StringBuilder();
+
+            builder.Append("StringBuilder ").Append("является ").Append("очень ... ");
+
+            string build1 = builder.ToString();
+
+            builder.Append("быстрым");
+
+            string build2 = builder.ToString();
+
+            Console.WriteLine(build1);
+            Console.WriteLine(build2);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
