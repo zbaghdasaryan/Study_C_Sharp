8dee45fa560873fc40c6322b8d210977beba17c4
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-24 17:50:29 +0400

    HomeWork 2

---
+++ study C shatp third stage/HomeWork 2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Text.RegularExpressions;
+
+namespace HomeWork 2
+{
+    class Program
+    {
+        public static string OnlyBrackets(string text)
+        {
+            string brackets = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == '{' || text[i] == '}' || text[i] == '['
+                    || text[i] == ']' || text[i] == '<' || text[i] == '>'
+                    || text[i] == '(' || text[i] == ')')
+                {
+                    brackets += text[i];
+                }
+            }
+            return brackets;
+        }
+
+        public static string IsBracketTrueOrFals(string onlyParenthesestext)
+        {
+            string bracketstext = OnlyBrackets(onlyParenthesestext);
+            while (bracketstext.Contains("{}") || bracketstext.Contains("[]")
+                || bracketstext.Contains("<>") || bracketstext.Contains("()"))
+            {
+                bracketstext = bracketstext.Replace("{}", "");
+                bracketstext = bracketstext.Replace("[]", "");
+                bracketstext = bracketstext.Replace("<>", "");
+                bracketstext = bracketstext.Replace("()", "");
+            }
+            if (bracketstext == null)
+                return "brackets are put in the right places";
+            else
+            {
+                Console.Write("brackets are put in the wrong place because there is ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(bracketstext);
+                return null;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            string text = "adfjkasd~#(*&$% .,//9[]/]   @_%^-_+?*-+fjl()af165!   #23  54af(dzksdj)kasjd<ss{}sdfs>)fl";
+
+            Console.WriteLine(text);
+            Console.WriteLine();
+            IsBracketTrueOrFals(text);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ study C shatp third stage/HomeWork 2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HomeWork 2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("HomeWork 2")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3f78d137-78bf-4d1d-b94a-789f3eee88af")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
