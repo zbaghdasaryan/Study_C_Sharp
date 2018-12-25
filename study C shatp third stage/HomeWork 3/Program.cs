6193657e7cd6bf15124d26a831e371237b9545b7
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2018-12-25 17:22:36 +0400

    HomeWork 3

---
+++ study C shatp third stage/HomeWork 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = @"dfjdhf(0.2569)a035{.6589}fgjk<2569>cvcvv[2365.0]cd";
+
+            ShowFloatingPoints(text);
+        }
+
+        private static void ShowFloatingPoints(string input)
+        {
+            string pattern = @"[(][-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?[)]|[{][-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?\}|[<][-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?\>|[[][-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?\]";
+
+            var collection = Regex.Matches(input, pattern);
+
+            MatchCollection floatNumberCollection = Regex.Matches(input, pattern);
+
+            foreach (var item in floatNumberCollection)
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+    }
+}
