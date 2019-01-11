a75bdd8fc69b7720f4099009f5c09dd555bc4fa3
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-11 03:05:44 -0800

    TypeTest 1

---
+++ study C shatp third stage/TypeTest 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+// Рефлексия. Пример получения экземпляра Type.
+
+namespace TypeTest
+{
+    class MyClass
+    {
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var my = new MyClass();
+            Type type;
+
+            // Способы получения экземрляра класса Type.
+
+            // 1.
+            type = my.GetType();
+            Console.WriteLine("1й способ: " + type);
+
+            // 2.
+            type = Type.GetType("TypeTest.MyClass"); // Полное квалифицированое имя типа в строковом представлении.
+            Console.WriteLine("2й способ: " + type);
+
+            // 3.
+            type = typeof(MyClass);
+            Console.WriteLine("3й способ: " + type);
+
+            // Delay.
+            Console.ReadKey();
+
+        }
+    }
+}
