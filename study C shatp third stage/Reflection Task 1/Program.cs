03383d582134fe430ebb758ca173583dd8e90952
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-12 11:15:50 -0800

    Reflection Task 1

---
+++ study C shatp third stage/Reflection Task 1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflection_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = Type.GetType("SomeClass");
+            ShowMetaData(t);
+        }
+
+        static void ShowMetaData(Type t)
+        {
+           
+            FieldInfo[] F = t.GetFields();
+            foreach (FieldInfo info in F)
+                Console.WriteLine(info.Name);
+            Console.ReadKey();
+
+            PropertyInfo[] P = t.GetProperties();
+            foreach (PropertyInfo info in P)
+                Console.WriteLine(info.Name);
+            Console.ReadKey();
+
+            MethodInfo[] M = t.GetMethods();
+            foreach (MethodInfo info in M)
+                Console.WriteLine(info.Name);
+            Console.ReadKey();
+
+            Type[] I = t.GetInterfaces();
+            foreach (Type info in I)
+                Console.WriteLine(info.Name);
+            Console.ReadKey();
+        }
+    }
+
+    interface ISomeInterfase
+    {
+        string SomeMethod(bool flag);
+    }
+    class BaseClass
+    {
+
+    }
+    class SomeClass : BaseClass, ISomeInterfase
+    {
+        int aaa;
+        public int Aaa
+        {
+            get { return aaa; }
+
+            set { aaa = value; }
+        }
+        public string SomeMethod(bool flag)
+        {
+            return flag.ToString();
+        }
+    }
+}
