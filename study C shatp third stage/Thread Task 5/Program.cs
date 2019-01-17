25d61acd410f13a42ab9ffac0580852c99e70a38
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-17 21:13:37 +0400

    Thread Task 5

---
+++ study C shatp third stage/Thread Task 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+// Потоки. Анонимные методы.
+
+namespace Threads
+{
+    class Program
+    {
+        static void Main()
+        {
+            int counter = 0;
+
+            // ThreadStart
+            Thread thread = new Thread(delegate () { Console.WriteLine("1. counter = {0}", ++counter); });
+            thread.Start();
+
+            Thread.Sleep(100);
+            Console.WriteLine("2. counter = {0}", counter);
+
+            // ParameterizedThreadStart
+            thread = new Thread((object argument) => { Console.WriteLine("3. counter = {0}", (int)argument); });
+            thread.Start(counter);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
