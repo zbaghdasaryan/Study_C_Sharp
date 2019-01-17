b5e07c4e4c8242dff91a5d7f3389f1bf8c118716
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-17 21:02:22 +0400

    Thread Task 4

---
+++ study C shatp third stage/Thread Task 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+// Потоки. Передача данных в поток.
+
+namespace Threads
+{
+    class Program
+    {
+        // Метод, который планируется выполнять в отдельном потоке.
+        static void WriteSecond(object argument)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(argument);
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void Main()
+        {
+            ParameterizedThreadStart writeSecond = new ParameterizedThreadStart(WriteSecond);
+            Thread thread = new Thread(writeSecond);
+            thread.Start("Hello");
+
+            Thread.Sleep(500);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
