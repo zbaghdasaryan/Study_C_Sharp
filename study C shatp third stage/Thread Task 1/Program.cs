10be8d64553a50952f69db389f00d85ad6b8fd0a
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-17 19:51:11 +0400

    Thread Task 2

---
+++ study C shatp third stage/Thread Task 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+
+namespace Threads
+{
+    class Program
+    {
+ 
+        static void WriteSecond()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+
+            
+                Console.WriteLine(new string(' ', 10) + "Secondary");
+            }
+        }
+
+        static void Main()
+        {
+            ThreadStart writeSecond = new ThreadStart(WriteSecond);
+            Thread thread = new Thread(writeSecond);
+            thread.Start();
+
+            for (int i = 0; i < 10; i++)             
+            {
+                Console.WriteLine("Primary");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ study C shatp third stage/Thread Task 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+// Потоки.
+
+namespace Threads
+{
+    class Program
+    {
+        // Статический метод, который планируется выполнять одновременно в главном (первичном) и во вторичном потоках.
+        static void WriteSecond()
+        {
+            // CLR назначает каждому потоку свой стек и методы имеют свои собственные локальные переменные.
+            // Отдельный экземпляр переменной counter создается в стеке каждого потока, 
+            // поэтому для каждого потока выводятся, свои значения counter - 0,1,2.
+            int counter = 0;
+
+            while (counter < 10)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("Thread Id {0}, counter = {1}", Thread.CurrentThread.GetHashCode(), counter);
+                counter++;
+            }
+        }
+
+        static void Main()
+        {
+            // Работа вторичного потока.
+            Thread thread = new Thread(WriteSecond);
+            thread.Start();
+
+            // Работа первичного потока.
+            WriteSecond();
+
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
