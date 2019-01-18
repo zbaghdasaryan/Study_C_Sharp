dd7ba98ef922ae4f969ed908a45541d086f4215e
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-18 16:49:56 +0400

    Thread Join Task 3

---
+++ study C shatp third stage/Thread Join Task 3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadSampleMul
+{
+    class Program
+    {
+        // Общая переменная счетчик.
+        //   [ThreadStatic] //TODO Снять комментарий
+        public static int counter;
+
+        // Рекурсивный запуск потоков.
+        public static void Method()
+        {
+            if (counter < 100)
+            {
+                counter++; // Увеличение счетчика вызваных методов.
+                Console.WriteLine(counter + " - СТАРТ --- " + Thread.CurrentThread.GetHashCode());
+                var thread = new Thread(Method);
+                thread.Start();
+                thread.Join(); // Закомментировать.               
+            }
+
+            Console.WriteLine("Поток {0} завершился.", Thread.CurrentThread.GetHashCode());
+        }
+
+        static void Main()
+        {
+            // Запуск вторичного потока.
+            var thread = new Thread(Method);
+            thread.Start();
+            thread.Join();
+
+            Console.WriteLine("Основной поток завершил работу...");
+
+            // Задержка.
+            Console.ReadKey();
+        }
+    }
+}
