c505de1e486f082aac81bc9273fc61ac01443dc2
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-21 13:42:00 +0400

    MergeSort

---
+++ study C shatp third stage/Algorithm_MergeSort/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    class Program
+    {
+        static public void MegreSort(int[] items)
+        {
+            // Если массив содержит один элемент - прервать выполнение метода  
+
+            if (items.Length <= 1)
+            {
+                return;
+            }
+
+            // 
+
+            int leftSize = items.Length / 2;
+            int rightSize = items.Length - leftSize;
+            int[] left = new int[leftSize];
+            int[] right = new int[rightSize];
+
+            Array.Copy(items, 0, left, 0, leftSize);
+            Array.Copy(items, leftSize, right, 0, rightSize);
+
+            // Рекурсивное деление массива
+
+            MegreSort(left);
+            MegreSort(right);
+
+            Merge(items, left, right);
+
+        }
+
+        static private void Merge(int[] items, int[] left, int[] right)
+        {
+            int leftIndex = 0;
+            int rightIndex = 0;
+            int targetIndex = 0;
+            int remaining = left.Length + right.Length; // общая длинна правой и левой части сортируемого массива
+
+            while (remaining > 0)
+            {
+                if (leftIndex >= left.Length)
+                {
+                    items[targetIndex] = right[rightIndex++];
+                }
+
+                else if (rightIndex >= right.Length)
+                {
+                    items[targetIndex] = left[leftIndex++];
+                }
+
+                else if (left[leftIndex].CompareTo(right[rightIndex]) < 0)
+                {
+                    items[targetIndex] = left[leftIndex++];
+                }
+
+                else
+                {
+                    items[targetIndex] = right[rightIndex++];
+                }
+
+                targetIndex++;
+                remaining--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            // Создание массива
+
+            int[] array = { 3, 8, 2, 1, 5, 4, 6, 7 };
+
+            // Сортировка массива
+
+            MegreSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+
+    }
+}
--- study C shatp third stage/Algorithm_SelectionSort/Program.cs
+++ study C shatp third stage/Algorithm_SelectionSort/Program.cs
@@ -4,12 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Algorithm_SelectionSort
+namespace Selection_Sort
 {
     class Program
     {
+
+        // Метод переставляе местами две ячейки массива
+
+        static void Swap(int[] items, int left, int right)
+        {
+            if (left != right)
+            {
+                int temp = items[left];
+                items[left] = items[right];
+                items[right] = temp;
+            }
+        }
+
+        // Метод сортировки вставками 
+
+        static public void SelectionSort(int[] items)
+        {
+            int sortedRangeEnd = 0;
+
+            while (sortedRangeEnd < items.Length)
+            {
+                int nextIndex = FindIndexOfSmallestFromIndex(items, sortedRangeEnd);
+                Swap(items, sortedRangeEnd, nextIndex);
+                sortedRangeEnd++;
+            }
+        }
+
+        // Метод находит минимальный элемент в массиве.
+
+        static private int FindIndexOfSmallestFromIndex(int[] items, int sortedRangeEnd)
+        {
+            int currentSmallest = items[sortedRangeEnd];
+            int currentSmallestIndex = sortedRangeEnd;
+
+            for (int i = sortedRangeEnd + 1; i < items.Length; i++)
+            {
+                if (currentSmallest.CompareTo(items[i]) > 0)
+                {
+                    currentSmallest = items[i];
+                    currentSmallestIndex = i;
+                }
+            }
+
+            return currentSmallestIndex;
+        }
+
         static void Main(string[] args)
         {
+            int[] array = { 3, 7, 4, 4, 6, 5, 8 };
+
+            SelectionSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
         }
     }
 }
