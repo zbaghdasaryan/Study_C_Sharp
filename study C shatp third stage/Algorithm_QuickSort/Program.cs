46730eddd2087a09b3b5fbaa723b0ec5d428dab4
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-22 17:06:13 +0400

    Binary Three

---
+++ study C shatp third stage/Algorithm_QuickSort/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+
+    class Program
+    {
+        // Метод переставляе местами две ячейки массива
+
+        static void Swap(int[] items, int left, int right)
+        {
+            if (left != right)
+            {
+                int temp = items[left];
+                items[left] = items[right];
+                items[right] = temp;
+            }
+        }
+
+        // Метод быстрой сортировки
+
+        static public void QuickSort(int[] items)
+        {
+            quicksort(items, 0, items.Length - 1);
+        }
+
+        static private void quicksort(int[] items, int left, int right)
+        {
+            Random _pivotRng = new Random();
+
+            if (left < right)
+            {
+                // Генератор псевдослучайных чисел. Определение точки раздела массива.               
+                int pivotIndex = _pivotRng.Next(left, right);
+
+                int newPivot = partition(items, left, right, pivotIndex);
+                quicksort(items, left, newPivot - 1);
+                quicksort(items, newPivot + 1, right);
+            }
+        }
+
+        static private int partition(int[] items, int left, int right, int pivotIndex)
+        {
+            int pivotValue = items[pivotIndex];
+
+            Swap(items, pivotIndex, right);
+
+            int storeIndex = left;
+
+            for (int i = left; i < right; i++)
+            {
+                if (items[i].CompareTo(pivotValue) < 0)
+                {
+                    Swap(items, i, storeIndex);
+                    storeIndex += 1;
+                }
+            }
+
+            // Перестановка значений местами.
+            Swap(items, storeIndex, right);
+            return storeIndex;
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Алгоритм быстрой сортировки");
+
+            int[] array = { 3, 7, 4, 4, 6, 5, 8, 12, 19, 2, 0 };
+
+            QuickSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+
+        }
+    }
+}
---
+++ study C shatp third stage/Binary_Three/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_tree
+{
+    class BinaryTreeNode<TNode> : IComparable<TNode> where TNode : IComparable<TNode>
+    {
+        public BinaryTreeNode(TNode value)
+        {
+            Value = value;
+        }
+
+        public BinaryTreeNode<TNode> Left
+        {
+            get;
+            set;
+        }
+
+        public BinaryTreeNode<TNode> Right
+        {
+            get;
+            set;
+        }
+
+        public TNode Value
+        {
+            get;
+            private set;
+        }
+
+        // Метод сравнивает значение двух узлов (Value и other), если Value меньше, то возвращается значение меньше нуля.
+        //                                                       если они равны, то возвращается ноль,
+        //                                                       если Value больше, то возвращается значение больше нуля.
+
+        public int CompareTo(TNode other)
+        {
+            return Value.CompareTo(other);
+        }
+
+        public int CompareNode(BinaryTreeNode<TNode> other)
+        {
+            return Value.CompareTo(other.Value);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {                                                             //               5
+            BinaryTreeNode<int> Node1 = new BinaryTreeNode<int>(5);  //              / \
+            BinaryTreeNode<int> Node2 = new BinaryTreeNode<int>(3);  //             /   \
+            BinaryTreeNode<int> Node3 = new BinaryTreeNode<int>(8);  //            3     8
+
+
+            if (Node1.CompareNode(Node2) >= 0)
+            {
+                Node1.Left = Node2;
+            }
+
+            else
+            {
+                Node1.Right = Node2;
+            }
+
+
+            if (Node1.CompareNode(Node3) >= 0)
+            {
+                Node1.Left = Node3;
+            }
+
+            else
+            {
+                Node1.Right = Node3;
+            }
+
+            Console.WriteLine("Левый потомок узла 5: {0}", Node1.Left.Value);
+            Console.WriteLine("Правый потомок узла 5: {0}", Node1.Right.Value);
+
+        }
+    }
+}
+
+
