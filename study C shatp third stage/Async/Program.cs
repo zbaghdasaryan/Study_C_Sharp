6106efeb9575669ffe691ea11beeed8e597a441b
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-01-29 14:27:31 +0400

    Asinc

---
+++ study C shatp third stage/Async/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Factorial()
+        {
+            int result = 1;
+            for (int i = 1; i <= 6; i++)
+            {
+                result *= i;
+            }
+            Thread.Sleep(8000);
+            Console.WriteLine($"Факториал равен {result}");
+        }
+        // определение асинхронного метода
+        static async void FactorialAsync()
+        {
+            Console.WriteLine("Начало метода FactorialAsync"); // выполняется синхронно
+            await Task.Run(() => Factorial());                            // выполняется асинхронно
+            Console.WriteLine("Конец метода FactorialAsync");  // выполняется синхронно
+        }
+
+        static void Main(string[] args)
+        {
+            FactorialAsync();   // вызов асинхронного метода
+
+            Console.WriteLine("Введите число: ");
+            int n = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Квадрат числа равен {n * n}");
+
+            Console.Read();
+        }
+    }
+}
