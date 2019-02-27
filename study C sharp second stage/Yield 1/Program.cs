9d96362e6bf5bcc6c1043fdf4ad5762bd31526ab
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-27 23:58:27 +0400

    Yield1

---
+++ study C sharp second stage/Yield 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers numbers = new Numbers();
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Numbers
+    {
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                yield return i * i;
+            }
+        }
+    }
+}
