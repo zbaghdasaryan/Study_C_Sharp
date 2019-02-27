08f163827bae8bb2c665257b31bee1e1a074b383
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-27 23:59:13 +0400

    Yield2

---
+++ study C sharp second stage/Yield2/Program.cs
@@ -0,0 +1,32 @@
+﻿class Book
+{
+    public Book(string name)
+    {
+        this.Name = name;
+    }
+    public string Name { get; set; }
+}
+
+class Library
+{
+    private Book[] books;
+
+    public Library()
+    {
+        books = new Book[] { new Book("Отцы и дети"), new Book("Война и мир"),
+                new Book("Евгений Онегин") };
+    }
+
+    public int Length
+    {
+        get { return books.Length; }
+    }
+
+    public IEnumerator GetEnumerator()
+    {
+        for (int i = 0; i < books.Length; i++)
+        {
+            yield return books[i];
+        }
+    }
+}
