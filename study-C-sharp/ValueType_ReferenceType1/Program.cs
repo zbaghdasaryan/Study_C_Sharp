a329304222d2853aa4977faf765ebe82ddcadf86
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-08 16:45:37 +0400

    ValueType_ReferenceType1

---
+++ study-C-sharp/ValueType_ReferenceType1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ValueType_ReferenceType1
+{
+    struct S
+    {
+        public int x;
+    }
+
+    class C
+    {
+        public int x;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            S s1 = new S();
+            S s2 = new S();
+
+            Console.WriteLine("Structura");
+
+            s2.x = 1;
+            Console.WriteLine("s2.x = " + s2.x);
+
+            s1 = s2;
+            s2.x = 3;
+
+            Console.WriteLine("s1.x = " + s1.x);
+            Console.WriteLine("s2.x = " + s2.x);
+
+
+            C c1 = new C();
+            C c2 = new C();
+            Console.WriteLine();
+            Console.WriteLine("Class");
+
+            c2.x = 1;
+            Console.WriteLine("c2.x = " + c2.x);
+
+            c1 = c2;
+            c2.x = 2;
+
+            Console.WriteLine("c1.x = " + c1.x);
+            Console.WriteLine("c2.x = " + c2.x);
+
+
+            Console.ReadLine();
+        }
+    }
+}
