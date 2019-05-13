2cee52a65eeac5221a5c9d66e274090fc1ddfa20
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-13 13:35:53 +0400

    Questions

---
+++ study-C-sharp/questions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //Сколько раз в этом цикле будет выполняться строка i = j - i;
+            int j = 2;
+            for (int i = 2; i < 32; i = i * 2)
+            {
+                while (i < j)
+                {
+                    j = j * 2;
+                }
+                i = j - i;
+            }
+        }
+    }
+}
