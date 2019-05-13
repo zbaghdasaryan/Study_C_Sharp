2919f755c8928028438206cc487356a84545e23d
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-13 13:38:45 +0400

    Questions

--- study-C-sharp/questions/Program.cs
+++ study-C-sharp/questions/Program.cs
@@ -7,15 +7,26 @@ namespace Questions
         static void Main(string[] args)
         {
 
-           //Сколько раз в этом цикле будет выполняться строка i = j - i;
-            int j = 2;
-            for (int i = 2; i < 32; i = i * 2)
+            //Сколько раз в этом цикле будет выполняться строка i = j - i;
+            //int j = 2;
+            //for (int i = 2; i < 32; i = i * 2)
+            //{
+            //    while (i < j)
+            //    {
+            //        j = j * 2;
+            //    }
+            //    i = j - i;
+            //}
+
+            //Что будет выведено на консоль в результате выполнения следующего цикла:
+            for (int i = 1; i < 3; i++)
             {
-                while (i < j)
+                switch (i)
                 {
-                    j = j * 2;
+                    default:
+                        Console.WriteLine($"i = {i++}");
+                        break;
                 }
-                i = j - i;
             }
         }
     }
