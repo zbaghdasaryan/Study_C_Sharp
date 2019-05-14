427654e59ba05927747a092cd1520a85b6bae83c
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-14 21:26:47 +0400

    Questions

--- study-C-sharp/questions/Program.cs
+++ study-C-sharp/questions/Program.cs
@@ -19,15 +19,52 @@ namespace Questions
             //}
 
             //Что будет выведено на консоль в результате выполнения следующего цикла:
-            for (int i = 1; i < 3; i++)
-            {
-                switch (i)
-                {
-                    default:
-                        Console.WriteLine($"i = {i++}");
-                        break;
-                }
-            }
+            //for (int i = 1; i < 3; i++)
+            //{
+            //    switch (i)
+            //    {
+            //        default:
+            //            Console.WriteLine($"i = {i++}");
+            //            break;
+            //    }
+            //}
+
+            //Какое число(или какие числа) выведет на консоль следующая программа и почему:
+            //    int i = 0;
+
+            //    Console.Write($"\ni1={i++} + \ncalc={Calculate(i)}");
+            //    Console.WriteLine("\ncw i= "+i);
+
+            //    Console.ReadKey();
+            //}
+
+            //public static int Calculate(int i)
+            //{
+            //    Console.Write("\ncalc cw="+i++);
+            //    return i;
+
+            //Какие из следующих вариантов представляют правильный способ вызова данного метода(правильных вариантов может быть несколько):
+        //    Display();
+   
+        //    Display(1);
+
+        //    Display(1, 2);
+
+        //    Display(1, 2, 3);
+
+        //    Display(new int[] { 1, 2, 3 });
+        //}
+        //static void Display(params int[] numbers)
+        //{
+        //    int result = 0;
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        result += numbers[i];
+        //    }
+        //    Console.WriteLine(result);
         }
+
+
     }
 }
+
