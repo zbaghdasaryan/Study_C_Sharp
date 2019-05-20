17fd9d9e9290ba4e32d02f12dee499c48fdb712b
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-20 12:26:05 +0400

    Questions_2

---
+++ study-C-sharp/Questions 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Questions_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            B obj2 = new B();
+            obj2.Foo();
+
+            A obj3 = new B();
+            obj3.Foo();
+        }
+    }
+
+    //class A
+    //{
+    //    public virtual void Foo()
+    //    {
+    //        Console.Write("Class A");
+    //    }
+    //}
+    //class B : A
+    //{
+    //    public override void Foo()
+    //    {
+    //        Console.Write("Class B");
+    //    }
+    //}
+    public struct S : IDisposable
+    {
+        private bool dispose;
+        public void Dispose()
+        {
+            dispose = true;
+        }
+        public bool GetDispose()
+        {
+            return dispose;
+        }
+    }
+}
