9bb0e1dd02d46ccd0a260a7917c4cdae28c381e7
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-20 12:45:10 +0400

    Questions_2

--- study-C-sharp/Questions 2/Program.cs
+++ study-C-sharp/Questions 2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Questions_2
 {
@@ -6,14 +7,74 @@ namespace Questions_2
     {
         static void Main(string[] args)
         {
-           
-            B obj2 = new B();
-            obj2.Foo();
 
-            A obj3 = new B();
-            obj3.Foo();
+            //B obj2 = new B();
+            //obj2.Foo();
+
+            //A obj3 = new B();
+            //obj3.Foo();
+
+
+            //var s = new S();
+            //using (s)
+            //{
+            //    Console.WriteLine(s.GetDispose());
+            //}
+            //Console.WriteLine(s.GetDispose());
+
+
+            //List<Action> actions = new List<Action>();
+            //for (var count = 0; count < 10; count++)
+            //{
+            //    actions.Add(() => Console.WriteLine(count));
+            //}
+            //foreach (var action in actions)
+            //{
+            //    action();
+            //}
+
+            //int i = 1;
+            //object obj = i;
+            //++i;
+            //Console.WriteLine(i);
+            //Console.WriteLine(obj);
+            //Console.WriteLine((int)obj);
+
+            var c = new C();
+            A a = c;
+
+            a.Print2();
+            a.Print1();
+            c.Print2();
         }
     }
+
+    public class A
+    {
+        public virtual void Print1()
+        {
+            Console.Write("A");
+        }
+        public void Print2()
+        {
+            Console.Write("A");
+        }
+    }
+    public class B : A
+    {
+        public override void Print1()
+        {
+            Console.Write("B");
+        }
+    }
+    public class C : B
+    {
+        new public void Print2()
+        {
+            Console.Write("C");
+        }
+    }
+
 
     //class A
     //{
@@ -29,16 +90,16 @@ namespace Questions_2
     //        Console.Write("Class B");
     //    }
     //}
-    public struct S : IDisposable
-    {
-        private bool dispose;
-        public void Dispose()
-        {
-            dispose = true;
-        }
-        public bool GetDispose()
-        {
-            return dispose;
-        }
-    }
+    //public struct S : IDisposable
+    //{
+    //    private bool dispose;
+    //    public void Dispose()
+    //    {
+    //        dispose = true;
+    //    }
+    //    public bool GetDispose()
+    //    {
+    //        return dispose;
+    //    }
+    //}
 }
