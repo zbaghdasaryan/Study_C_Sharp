2964b17bb92455ef5e08e026df64a2f3c53da39a
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-05-22 18:42:08 +0400

    Questions_2

--- study-C-sharp/Questions 2/Program.cs
+++ study-C-sharp/Questions 2/Program.cs
@@ -40,42 +40,44 @@ namespace Questions_2
             //Console.WriteLine(obj);
             //Console.WriteLine((int)obj);
 
-            var c = new C();
-            A a = c;
+            //var c = new C();
+            //A a = c;
 
-            a.Print2();
-            a.Print1();
-            c.Print2();
+            //a.Print2();
+            //a.Print1();
+            //c.Print2();
+         
         }
     }
 
-    public class A
-    {
-        public virtual void Print1()
-        {
-            Console.Write("A");
-        }
-        public void Print2()
-        {
-            Console.Write("A");
-        }
-    }
-    public class B : A
-    {
-        public override void Print1()
-        {
-            Console.Write("B");
-        }
-    }
-    public class C : B
-    {
-        new public void Print2()
-        {
-            Console.Write("C");
-        }
-    }
+    //public class A
+    //{
+    //    public virtual void Print1()
+    //    {
+    //        Console.Write("A");
+    //    }
+    //    public void Print2()
+    //    {
+    //        Console.Write("A");
+    //    }
+    //}
+    //public class B : A
+    //{
+    //    public override void Print1()
+    //    {
+    //        Console.Write("B");
+    //    }
+    //}
+    //public class C : B
+    //{
+    //    new public void Print2()
+    //    {
+    //        Console.Write("C");
+    //    }
+    //}
 
-
+        
+    }
     //class A
     //{
     //    public virtual void Foo()
