0484457fbf706acdd34b33cf08ddbfcaece41693
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-01 21:54:02 +0400

    Questions_2

--- study-C-sharp/Questions 2/Program.cs
+++ study-C-sharp/Questions 2/Program.cs
@@ -9,8 +9,8 @@ namespace Questions_2
         static void Main(string[] args)
         {
 
-            B obj2 = new B();
-            obj2.Foo();
+            //B obj2 = new B();
+            //obj2.Foo();
 
             //A obj3 = new B();
             //obj3.Foo();
