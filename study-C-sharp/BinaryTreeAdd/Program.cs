b729ef22725da6bc2c77620a3f501eb13a7bafbd
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-02 17:00:10 +0400

    BinaryTreeAdd

---
+++ study-C-sharp/BinaryTreeAdd/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InOrderTraversal
+{
+    class BinaryTreeNode<TNode> : IComparable<TNode> where TNode : IComparable<TNode>
+    {
+        public BinaryTreeNode(TNode value)
+        {
+            Value = value;
+        }
+        public BinaryTreeNode<TNode> Left
+        {
+            get;
+            set;
+        }
+
+        public BinaryTreeNode<TNode> Right
+        {
+            get;
+            set;
+        }
+
+        public TNode Value
+        {
+            get;
+            private set;
+        }
+
+        public int CompareTo(TNode other)
+        {
+            return Value.CompareTo(other);
+        }
+
+    }
+
+    public class BinaryTree<T> : IEnumerable<T> where T : IComparable<T>
+    {
+        private BinaryTreeNode<T> _head;
+
+        private int _count;
+
+        #region Добавление нового узла дерева
+
+        public void Add(T value)
+        {
+            // Первый случай: дерево пустое     
+
+            if (_head == null)
+            {
+                _head = new BinaryTreeNode<T>(value);
+            }
+
+            // Второй случай: дерево не пустое, поэтому применяем рекурсивный алгорит 
+            //                для поиска места добавления узла        
+
+            else
+            {
+                AddTo(_head, value);
+            }
+            _count++;
+        }
+
+        // Рекурсивный алгоритм 
+
+        private void AddTo(BinaryTreeNode<T> node, T value)
+        {
+            // Первый случай: значение добавляемого узла меньше чем значение текущего. 
+
+            if (value.CompareTo(node.Value) < 0)
+            {
+                // если левый потомок отсутствует - добавляем его          
+
+                if (node.Left == null)
+                {
+                    node.Left = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    // повторная итерация               
+                    AddTo(node.Left, value);
+                }
+            }
+            // Второй случай: значение добавляемого узла равно или больше текущего значения      
+            else
+            {
+                // Если правый потомок отсутствует - добавлем его.          
+
+                if (node.Right == null)
+                {
+                    node.Right = new BinaryTreeNode<T>(value);
+                }
+                else
+                {
+                    // повторная итерация
+
+                    AddTo(node.Right, value);
+                }
+            }
+        }
+
+
+
+        #endregion
+
+        #region Нумератор
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        //public IEnumerator<T> GetEnumerator()
+        //{
+        //    return InOrderTraversal();
+        //}
+
+        //System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        //{
+        //    return GetEnumerator();
+        //}
+
+        #endregion
+
+        #region Количество узлов в дереве
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        #endregion
+
+      
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BinaryTree<int> instance = new BinaryTree<int>();
+
+            instance.Add(8);    //                        8
+            instance.Add(5);    //                      /   \
+            instance.Add(12);   //                     5    12 
+            instance.Add(3);    //                    / \   / \  
+            instance.Add(7);    //                   3   7 10 15
+            instance.Add(10);   //
+            instance.Add(15);   //
+
+            Console.WriteLine(instance.Count);
+            Console.ReadKey();
+
+        }
+    }
+}
