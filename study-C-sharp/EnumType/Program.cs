38e7738215d9ebaab5bbc9fdcdf3a2bfcd4b63fc
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-03 13:37:52 +0400

    EnumType

---
+++ study-C-sharp/EnumType/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace EnumType
+{
+    enum EnumType : byte
+    {
+        Zero,
+        One=1,
+        one=One,
+        Two=2,
+        Three,
+        Four=4,
+        Five=5,
+        Seven=Nine,
+        Eight=8,
+        Nine=9,
+        Infinite=255,
+               
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnumType x = EnumType.Five;
+            EnumType y = EnumType.Four;
+
+            Array array = Enum.GetValues(typeof(EnumType));
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("{0}   {0:D} ", array.GetValue(i));
+            }
+            if (x>y)
+                Console.WriteLine("x>y");
+            else
+                Console.WriteLine("x<=y");
+        }
+    }
+}
