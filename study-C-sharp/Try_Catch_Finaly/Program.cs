520bba17a5325f01e5e986bbe839bebbc35d546e
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-06 19:05:57 +0400

    Try_Catch_Finaly

---
+++ study-C-sharp/Try_Catch_Finaly/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Try_Catch_Finaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 5;
+                int y = x / 0;
+                Console.WriteLine($"Результат: {y}");
+            }
+            catch
+            {
+                Console.WriteLine("Возникло исключение!");
+            }
+            finally
+            {
+                Console.WriteLine("Блок finally");
+            }
+            Console.WriteLine("Конец программы");
+            Console.Read();
+        }
+    }
+}
