3a604de89e187d73e6ce9ebdfeecc9735f803e78
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-09 21:20:56 +0400

    Try_Catch_Finaly

--- study-C-sharp/Try_Catch_Finaly/Program.cs
+++ study-C-sharp/Try_Catch_Finaly/Program.cs
@@ -8,9 +8,7 @@ namespace Try_Catch_Finaly
         {
             try
             {
-                int x = 5;
-                int y = x / 0;
-                Console.WriteLine($"Результат: {y}");
+                
             }
             catch
             {
