a20b01d9d43df32be0c5979375199ab87eaf1735
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-15 19:38:46 +0400

    Thread_ForeGround

---
+++ study-C-sharp/Thread_ForeGround/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_ForeGround
+{
+    class Program
+    {
+        private static void Function()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write('.');
+            }
+            Console.WriteLine("\nThe secondary Thread ended");
+        }
+
+        static void Main(string[] args)
+        {
+            var thread = new Thread(Function);
+           // thread.IsBackground = false;
+            thread.Start();
+            Thread.Sleep(500);
+            Console.WriteLine("\nThe primary Thread ended");
+        }
+    }
+}
