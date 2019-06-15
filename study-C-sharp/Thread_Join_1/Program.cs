7ad9be4d4e851cbfdc9b8c6ec2e8e46d1987d417
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-15 14:02:50 +0400

    Thread_Join_1

---
+++ study-C-sharp/Thread_Join_1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace Thread_Join_1
+{
+    class Program
+    {
+        public static void ThreadFunc()
+        {
+            Console.WriteLine("ID for secondary Thread is: " + Thread.CurrentThread.ManagedThreadId);
+            Console.ForegroundColor = ConsoleColor.Red;
+            for (int i = 0; i < 160; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write(".");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\nSecondory thread is finish");
+
+        }
+        static void Main(string[] args)
+        {
+            var thread = new Thread(ThreadFunc);
+            Console.WriteLine("ID for primary Thread is:" + Thread.CurrentThread.GetHashCode());
+            Console.WriteLine("Start new Thread");
+            thread.Start();
+            Console.WriteLine(Thread.CurrentThread.GetHashCode());
+
+            thread.Join();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < 160; i++)
+            {
+                Thread.Sleep(20);
+                Console.Write("-");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("\nPrimary thread is finish");
+            Console.ReadKey();
+        }
+    }
+}
