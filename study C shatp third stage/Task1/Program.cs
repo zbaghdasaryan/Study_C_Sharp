cec2b737b4f46b1edede849e467315dc68788c2e
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-23 22:55:15 +0400

    Task1

---
+++ study C shatp third stage/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task Id "+ Thread.CurrentThread.ThreadState);
+            Task task1 = new Task(() => Console.WriteLine("Task1"));
+            task1.Start();
+
+            Console.WriteLine("Task Id " + Thread.CurrentThread.ThreadState);
+            Console.WriteLine("Task Id " + Thread.CurrentThread.GetHashCode());
+            Task task2 = Task.Factory.StartNew(() => Console.WriteLine("Task2"));
+
+            Console.WriteLine("Task Id " + Thread.CurrentThread.GetHashCode());
+            Task task3 = Task.Run(() => Console.WriteLine("Task3"));
+            Console.ReadLine();
+        }
+    }
+}
