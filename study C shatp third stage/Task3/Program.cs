87dc3a5312692701b96078455ce69427613551cf
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-25 13:36:25 +0400

    Task3

---
+++ study C shatp third stage/Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var outer = Task.Factory.StartNew(() =>      // внешняя задача
+            {
+                Console.WriteLine("Outer task starting...");
+                var inner = Task.Factory.StartNew(() =>  // вложенная задача
+                {
+                    Console.WriteLine("Inner task starting...");
+                    Thread.Sleep(2000);
+                    Console.WriteLine("Inner task finished.");
+                }, TaskCreationOptions.AttachedToParent);
+            });
+           outer.Wait(); // ожидаем выполнения внешней задачи
+            Console.WriteLine("End of Main");
+
+            Console.ReadLine();
+
+        }
+    }
+}
