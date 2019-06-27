52e7d24407e37a195c7819b10ccc633173d341d5
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-06-27 12:28:25 +0400

    Task5

---
+++ study C shatp third stage/Task5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = new Task(() => {
+                Console.WriteLine("Id задачи: {0}", Task.CurrentId);
+            });
+
+            // задача продолжения
+            Task task2 = task1.ContinueWith(Display);
+
+            task1.Start();
+
+            // ждем окончания второй задачи
+           // task2.Wait();
+            Console.WriteLine("Выполняется работа метода Main");
+            Console.ReadLine();
+        }
+
+        static void Display(Task t)
+        {
+            Console.WriteLine("Id задачи: {0}", Task.CurrentId);
+            Console.WriteLine("Id предыдущей задачи: {0}", t.Id);
+            Thread.Sleep(3000);
+        }
+    }
+}
